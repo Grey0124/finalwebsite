51532e0aa3aec4e1a97c867635c237bdd350490d
Grey0124 <161997693+Grey0124@users.noreply.github.com>
2025-02-14 18:07:58 +0530

    First commit
    Created website layout

---
+++ lib/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'The Makers Market'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'Artisan shopp and creative classes'
