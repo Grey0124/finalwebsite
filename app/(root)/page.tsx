939bb8934d3c8164e5ce903f39dc9d0f4bcd9d9e
Grey0124 <161997693+Grey0124@users.noreply.github.com>
2025-02-14 19:03:38 +0530

    List products
    sample product data created and displayed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,12 @@
-import { Button } from "@/components/ui/button";
+import ProductList from '@/components/shared/product/product-list'
+import sampleData from '@/lib/sample-data'
 
 export default function Home() {
   return (
-    <Button>Button</Button>
-
-  );
+    
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList data={sampleData.products} />
+    </div>
+  )
 }
