8913e12ab33ab648162d4de92837391f76ae208b
Grey0124 <161997693+Grey0124@users.noreply.github.com>
2025-02-20 21:11:44 +0530

    Created place order

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,10 @@
 import { carts, products } from '@/db/schema'
 import { InferSelectModel } from 'drizzle-orm'
-import { cartItemSchema, shippingAddressSchema } from '@/lib/validator'
+import {
+    cartItemSchema,
+    paymentResultSchema,
+    shippingAddressSchema,
+  } from '@/lib/validator'
 import { z } from 'zod'
 
 // PRODUCTS
@@ -11,3 +15,4 @@ export type Cart = InferSelectModel<typeof carts>
 export type CartItem = z.infer<typeof cartItemSchema>
 
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>
+export type PaymentResult = z.infer<typeof paymentResultSchema>
