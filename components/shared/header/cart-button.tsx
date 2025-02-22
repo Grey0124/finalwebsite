f5ac625a327d460a8e2c814c506597b5940e3ea8
Grey0124 <161997693+Grey0124@users.noreply.github.com>
2025-02-22 23:53:57 +0530

    linked external crowd funding app

--- components/shared/header/cart-button.tsx
+++ components/shared/header/cart-button.tsx
@@ -8,16 +8,26 @@ import { getMyCart } from '@/lib/actions/cart.actions'
 export default async function CartButton() {
   const cart = await getMyCart()
   return (
-    <Button asChild variant="ghost">
-      <Link href="/cart">
-        <ShoppingCart className="mr-1" />
-        Cart
-        {cart && cart.items.length > 0 && (
-          <Badge className="ml-1">
-            {cart.items.reduce((a, c) => a + c.qty, 0)}
-          </Badge>
-        )}
-      </Link>
-    </Button>
+    <div className="flex items-center gap-2">
+      {/* New Button for Web3 App */}
+      <Button asChild variant="ghost">
+        <Link href="https://web3cfapp-six.vercel.app/" target="_blank">
+          Crowd Funding
+        </Link>
+      </Button>
+
+      {/* Cart Button */}
+      <Button asChild variant="ghost">
+        <Link href="/cart">
+          <ShoppingCart className="mr-1" />
+          Cart
+          {cart && cart.items.length > 0 && (
+            <Badge className="ml-1">
+              {cart.items.reduce((a, c) => a + c.qty, 0)}
+            </Badge>
+          )}
+        </Link>
+      </Button>
+    </div>
   )
 }
