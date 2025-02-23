18cca1fa89338afdff027e86b6d36f87390ddfbe
Grey0124 <161997693+Grey0124@users.noreply.github.com>
2025-02-23 17:07:24 +0530

    adjusted carousel size

--- components/shared/product/product-carousel.tsx
+++ components/shared/product/product-carousel.tsx
@@ -30,17 +30,17 @@ export default function ProductCarousel({ data }: { data: Product[] }) {
         {data.map((product: Product) => (
           <CarouselItem key={product.id}>
             <Link href={`/product/${product.slug}`}>
-              <div className="relative   mx-auto  ">
+              <div className="relative mx-auto max-w-full">
                 <Image
                   alt={product.name}
                   src={product.banner!}
                   width="0"
                   height="0"
                   sizes="100vw"
-                  className="w-full h-auto"
+                  className="w-full h-auto max-h-[500px] object-contain"
                 />
                 <div className="absolute inset-0 flex items-end justify-center">
-                  <h2 className="bg-gray-900 bg-opacity-50 text-2xl font-bold px-2 text-white  ">
+                  <h2 className="bg-gray-900 bg-opacity-50 text-2xl font-bold px-2 text-white mb-4">
                     {product.name}
                   </h2>
                 </div>
